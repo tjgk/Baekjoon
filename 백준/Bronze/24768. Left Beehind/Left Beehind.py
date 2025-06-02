cc503354fe087740504d410fd347e4f897acb3fb
이서하 <70050285+tjgk@users.noreply.github.com>
2025-06-02 18:24:07 +0900

    [Bronze IV] Title: Left Beehind, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24768. Left Beehind/Left Beehind.py
@@ -0,0 +1,12 @@
+while 1:
+    x,y=map(int,input().split())
+    if x+y==0:
+        break
+    if x+y==13:
+        print("Never speak again.")
+    elif x<y:
+        print("Left beehind.")
+    elif x>y:
+        print("To the convention.")
+    else:
+        print("Undecided.")
