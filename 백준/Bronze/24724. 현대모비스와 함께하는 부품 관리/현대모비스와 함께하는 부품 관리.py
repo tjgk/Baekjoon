b2f377fe1b64fdb36a5cae3ead0e4c1e65efbbc6
이서하 <70050285+tjgk@users.noreply.github.com>
2025-11-01 12:19:13 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,10 @@
+import sys
+input=sys.stdin.readline
+
+for i in range(int(input())):
+    n=int(input())
+    a,b=input().split()
+    for j in range(n):
+        u,v=input().split()
+    print(f"Material Management {i+1}")
+    print("Classification ---- End!")
