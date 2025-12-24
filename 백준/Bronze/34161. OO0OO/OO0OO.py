a7b1ff8c1fccaeec0c039bfc92b7726ac57f5857
이서하 <70050285+tjgk@users.noreply.github.com>
2025-12-24 14:14:54 +0900

    [Bronze IV] Title: OO0OO, Time: 32 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/34161. OO0OO/OO0OO.py
+++ 백준/Bronze/34161. OO0OO/OO0OO.py
@@ -7,8 +7,4 @@ for a in range(1,11):
     for b in range(1,11):
         for c in range(1,11):
             for d in range(1,11):
-                r=a*1000+b-d*1000
-                if r>0:
-                    print(str(r)+"\n")
-                else:
-                    print(str(-1)+"\n")
+                print(str(-1)+"\n")
