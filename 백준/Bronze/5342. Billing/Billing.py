fc5615e4d91c4ac7897d70fd594af989c5812539
이서하 <70050285+tjgk@users.noreply.github.com>
2026-03-13 17:34:52 +0900

    [Bronze III] Title: Billing, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5342. Billing/Billing.py
@@ -0,0 +1,16 @@
+t={"Paper":57.99,
+"Printer":120.50,
+"Planners":31.25,
+"Binders":22.50,
+"Calendar":10.95,
+"Notebooks":11.20,
+"Ink":66.95}
+
+s=0
+while 1:
+    a=input()
+    if a=="EOI":
+        break
+    else:
+        s+=t[a]
+print(f"${s}")
